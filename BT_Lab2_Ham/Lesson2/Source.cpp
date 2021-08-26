875d89463608f90ba502624b85295a91f8cebc09
DuatTran268 <86725964+DuatTran268@users.noreply.github.com>
2021-08-26 23:50:28 +0700

    Upload file

--- BT_Lab2_Ham/Lesson2/Source.cpp
+++ BT_Lab2_Ham/Lesson2/Source.cpp
@@ -16,27 +16,33 @@ int main()
 {
 	// Viết chương trình cho phép nhập vào số giây (ký hiệu là n) và sau đó quy đổi thời gian giây thành
 	//giờ, phút, giây.Xuất kết quả ra màn hình dưới dạng : giờ:phút:giây. Ví dụ : người dùng nhập n = 3770 thìxuất ra màn hình 1 : 2 : 50
-	int soGiay;
-	double doitime, gio, phut, giay;
+	cout << endl << "\t CHUONG TRINH QUY DOI TIME";
+	// khai báo kiểu dữ liệu
+	int soGiay;					// sử dụng kdl gán 
+	double gio, phut, giay;
 	
-	cout << endl << " Nhap vao so giay can quy doi:  ";
+	//người dùng thấy và nhập vào
+	cout << endl << "\n\t\t\t Nhap vao so giay can quy doi:  ";
 	cin >> soGiay;
+	
+	// gọi hàm
+	DoiThoiGian(soGiay);
 
-	cout << endl << " So giay nhap vao la:  " << soGiay;
-	cout << endl << DoiThoiGian;
-
-
+	_getch();
 	return 1;
 }
 
 // định nghĩa hàm ( hàm ko trả về sử dụng kdl void)
 void DoiThoiGian(unsigned int n)
 {
-	double gio;
-	gio = n / MAX;
-	double phut;
-	phut = (n % MAX) / SIXTY;
-	double giay;
-	giay = (n % MAX) / SIXTY;
 	
+	double gio = n / MAX;
+	double phut = (n % MAX) / SIXTY;
+	double giay = (n % MAX) % SIXTY;
+
+	// xuất
+	cout << "\n\t So giay nhap vào n = "<< n << " duoc chuyen doi thanh: " << gio << "g : " << phut << "p : " << giay << "s";
+	cout << endl << "\n";
+
+
 }
--- BT_Lab2_Ham/Lessons10/Source.cpp
+++ BT_Lab2_Ham/Lessons10/Source.cpp
@@ -1,20 +1,22 @@
 ﻿// thư viện hàm
-#include<math.h>
 #include<iostream>
+#include<math.h>
 #include<conio.h>
 
-
-
 using namespace std;
+
 // khai báo nguyên mẫu hàm
-double TinhDoCao(double phi, double delta, double h, double sin, double cos);
-double TinhPhuongVi(double phi, double delta, double h, double altitude, double sin, double cos);
+double TinhDoCao(double phi, double delta, double h);
+double TinhPhuongVi(double phi, double delta, double h, double altitude);
+
+
 
 int main()
 {
 	// chuyển đổi hệ tọa độ
 	cout << endl << " Chuyen doi he toa do \n";
 	double phi, delta, h;
+	double altitude{};
 	double docao, phuongvi;
 	
 	cout << " Nhap vao xich vi do (delta):  ";
@@ -22,29 +24,29 @@ int main()
 	cout << " Nhap vao vi do (phi):  ";
 	cin >> phi;
 	cout << " Nhap vao goc cua mui gio (h):  ";
+	cin >> h;
 
 	// xuất
-	/*docao = TinhDoCao(phi, h, delta);
+	docao = TinhDoCao(phi, h, delta);
 	cout << endl << " Do cao la:  " << docao;
-	phuongvi = TinhPhuongVi(phi, delta, h);
-	cout << endl << " Phuong vi la:  " << phuongvi;*/
-
+	phuongvi = TinhPhuongVi(phi, delta, h, altitude);
+	cout << endl << " Phuong vi la:  " << phuongvi;
 
 	return 0;
 }
-// định nghĩa hàm
-//double TinhDoCao(double phi, double delta, double h,double sin,double cos)
-//{
-//	double Altitude;
-//	Altitude = asin(sin * phi * sin * delta + cos * phi * cos * delta * cos * h);
-//	return Altitude;
-//}
-//double TinhPhuongVi(double phi, double delta, double h, double altitude, double sin, double cos	)
-//{
-//	double Azimuth;
-//	Azimuth = asin((cos * phi * sin * delta - sin * phi * cos * delta * cos * h) / cos*(altitude));
-//	return Azimuth;
-//}
+
+double TinhDoCao(double phi, double delta, double h)
+{
+	double Altitude;
+	Altitude = asin(sin(phi)*sin(delta) + cos(phi)*cos(delta)*cos(h)); // ok rồi nè Duạt ơi :d
+	return Altitude;
+}
+double TinhPhuongVi(double phi, double delta, double h, double altitude	)
+{
+	double Azimuth;
+	Azimuth = acos((cos(phi)*cos(delta) - sin(phi)*cos(delta)*cos(h))/cos(altitude));
+	return Azimuth;
+}
 
 
 
