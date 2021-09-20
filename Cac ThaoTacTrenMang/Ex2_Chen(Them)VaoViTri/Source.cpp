6282e1ba2c97c3f648520f5f4480e50e62aff305
DuatTran268 <86725964+DuatTran268@users.noreply.github.com>
2021-09-20 20:54:00 +0700

    Upload file :-1:

---
+++ Cac ThaoTacTrenMang/Ex2_Chen(Them)VaoViTri/Source.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<conio.h>
+
+
+
+using namespace std;
+
+#include"ThuVien.h"
+#include"Menu.h"
+
+
+void ChayChuongTrinh();
+
+
+// hàm main()
+int main() {
+
+	ChayChuongTrinh();
+	return 0;
+}
+
+void ChayChuongTrinh()
+{
+	int menu, soMenu = 5;
+	int n = 0;
+	MANG a;
+	do
+	{
+		menu = ChonMenu(soMenu);
+		XuLyMenu(a, menu, n);
+
+	} while (menu > 0);
+
+}
+
+
