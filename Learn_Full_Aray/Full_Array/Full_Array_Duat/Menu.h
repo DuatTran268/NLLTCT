1d9a75bdfa6037ae473e4357549e8d51002b74da
DuatTran268 <duatsen36@gmail.com>
2021-11-25 01:21:12 +0700

    array element search

---
+++ Learn_Full_Aray/Full_Array/Full_Array_Duat/Menu.h
@@ -0,0 +1,150 @@
+#pragma once
+void XuatMenu() {
+	cout << endl << "\n --------- CAC KY THUAT XU LY MANG ------------";
+	cout << endl << " 0. Thoat chuong trinh";
+	cout << endl << " 1. Nhap mang tu dong";
+	cout << endl << " 2. Nhap mang bang tay";
+	cout << endl << " 3. Xuat mang da nhap";
+	// search array element
+	cout << endl << " 4. Tim vi tri cua phan tu x trong mang nhap vao";
+	cout << endl << " 5. Tim gia tri x trong mang";
+	cout << endl << " 6. Tim gia tri nho nhat trong mang";
+	cout << endl << " 7. Tim gia tri lon nhat trong mang";
+	cout << endl << " 8. Tim so le trong mang";
+	cout << endl << " 9. Tim so chan trong mang";
+	cout << endl << " 10. Tim so am trong mang";
+	cout << endl << " 11. Tim so duong trong mang";
+	cout << endl << " 12. Tim tat ca cac so nguyen to trong mang";
+	cout << endl << " 13. Tim tat ca cac so chinh phuong trong mang";
+
+	cout << endl << "\n-------------------- END ------------------";
+
+
+}
+
+int ChonMenu(int soMenu) {
+	int stt;
+	for (;;) {
+		system("cls");
+		XuatMenu();
+		cout << endl << "\n Nhap vao so tu 0 -> " << soMenu << " de chon chuc nang Menu:  ";
+		cin >> stt;
+		if (0 <= stt && stt <= soMenu)
+			break;
+	}
+	return stt;
+}
+
+void XuLyMenu(Array a, int menu, int& n) {
+	system("cls");
+	int kq, x;
+	int i = 0;
+	switch (menu)
+	{
+	case 0:
+		cout << endl << " 0. Thoat chuong trinh";
+		return;
+
+	case 1: 
+		cout << endl << " 1. Nhap mang tu dong";
+		NhapMangTuDong(a, n);
+		XuatMang(a, n);
+		break;
+		
+	case 2:
+		cout << endl << " 2. Nhap mang tu ban phim";
+		NhapMangTuKey(a, n);
+		XuatMang(a, n);
+		break;
+
+	case 3:
+		cout << endl << " 3. Xuat mang da nhap";
+		XuatMang(a, n);
+		break;
+
+	case 4:
+		cout << endl << " 4. Tim vi tri cua phan tu x nhap vao";
+		cout << endl << "\n Mang ban dau";
+		XuatMang(a, n);
+		cout << endl << "\n Nhap vao gia tri cua mang:  ";
+		cin >> x;
+		TimViTriCuaPhanTuXNhapVao(a, n, x);
+		break;
+
+	case 5:
+		cout << endl << "5. Tim nhap vao vi tri i xuat ra gia tri x trong mang";
+		cout << endl << "\n Xem mang ban dau";
+		XuatMang(a, n);
+		/*do
+		{
+		} while (i <= 0);*/
+		cout << endl << "\n Nhap vao vi tri i trong mang";
+		cin >> i;
+		NhapVaoViTriIXuatRaGiaTriX(a, n, x);
+		break;
+
+	case 6:
+		cout << endl << " 6. Tim gia tri nho nhat trong mang";
+		cout << endl << "\n Xem lai mang ban dau";
+		XuatMang(a, n);
+		kq = TimGiaTriNhoNhatTrongMang(a, n);
+		cout << endl << "\n Gia tri nho nhat trong mang la: " << kq;
+		break;
+
+	case 7:
+		cout << endl << " 7. Tim gia tri lon nhat trong mang";
+		cout << endl << "\n Xem laii mang ban dau";
+		XuatMang(a, n);
+		kq = TimGiaTriLonNhatTrongMang(a, n);
+		cout << endl << "\n Gia tri lon nhat trong mang la: " << kq;
+		break;
+
+	case 8:
+		cout << endl << " 8. Tim cac so le trong mang";
+		cout << endl << "\n Xem lai mang ban dau";
+		XuatMang(a, n);
+		TimSoLeTrongMang(a, n);
+		break;
+
+	case 9: 
+		cout << endl << "9. Tim cac chan trong mang";
+		cout << endl << "\n Xem lai mang ban dau";
+		XuatMang(a, n);
+		TimSoChanTrongMang(a, n);
+		break;
+
+	case 10:
+		cout << endl << " 10. Tim cac so am trong mang";
+		cout << endl << "\n Xem lai mang ban dau";
+		XuatMang(a, n);
+		TimSoAmTrongMang(a, n);
+		break;
+
+	case 11:
+		cout << endl << " 11. Tim cac so duong trong mang";
+		cout << endl << "\n Xem lai mang ban dau";
+		XuatMang(a, n);
+		TimSoDuongTrongMang(a, n);
+		break;
+
+	case 12:
+		cout << endl << " 12. Tim cac so nguyen to trong mang";
+		cout << endl << "\n Xem lai mang ban dau";
+		XuatMang(a, n);
+		XuatCacSoNguyenTo(a, n);
+		break;
+
+	case 13:
+		cout << endl << " 13. Tim cac so chinh phuong trong mang";
+		cout << endl << "\n Xem lai mang ban dau";
+		XuatMang(a, n);
+		XuatSoChinhPhuong(a, n);
+		break;
+
+
+	default:
+		break;
+	}
+	cout << endl << "\n";
+	system("pause");
+}
---
+++ Learn_Full_Aray/Full_Array/Full_Array_Duat/ThuVien.h
@@ -0,0 +1,169 @@
+﻿#pragma once
+#define MAX 100
+
+typedef int Array[MAX];
+
+
+void NhapMangTuDong(Array a, int& n) {
+	do
+	{
+		cout << endl << "\n Nhap vao so luong phan tu mang:  ";
+		cin >> n;
+	} while (n <= 0);
+	for (int i = 0; i < n; i++)
+	{
+		// công thức tính sinh số ngẫu nhiên
+		a[i] = rand() % (10 - -10 + 1) + -10;
+	}
+}
+
+void NhapMangTuKey(Array a, int& n) {
+	do
+	{
+		cout << endl << "\n Nhap vao so luong phan tu mang:  ";
+		cin >> n;
+	} while (n <= 0);
+	for (int i = 0; i < n; i++)
+	{
+		cout << endl << " Nhap a[" << i << "] =  ";
+		cin >> a[i];
+	}
+}
+
+
+void XuatMang(Array a, int n) {
+	cout << endl << "\n Xuat Mang:\t";
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << "\t";
+	}
+}
+
+// case 4: tìm vị trí của phần tử x trong mảng nhập vào
+void TimViTriCuaPhanTuXNhapVao(Array a, int n, int x) {
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] == x)
+		{
+			cout << endl << "\n Phan tu nhap vao x = " << x << " nam o vi tri i = " << i << " trong mang";
+		}
+	}
+}
+
+// case 5: tìm giá trị x trong mảng
+void NhapVaoViTriIXuatRaGiaTriX(Array a, int n, int x) {
+	for (int i = 0; i < n; i++)
+	{
+		if (i == x)
+		{
+			cout << endl << "\n Vi tri nhap vao i = " << i << " co gia tri x = " << x;
+		}
+	}
+}
+
+// case 6: Tìm giá trị nhỏ nhất trong mảng
+int TimGiaTriNhoNhatTrongMang(Array a, int n) {
+	// gán giả sử ban đầu biến min = vị trí 0 trong mảng a
+	int min = a[0];
+	// duyệt vòng lặp kiểm tra các phần tử trong mảng
+	for (int i = 0; i < n; i++)
+	{
+		// nếu tại vị trí min a[0] mà nhỏ hơn vị trí i trong mảng a
+		if (min > a[i]) {
+			min = a[i];		// thì min = chính vị trí i trong mảng a đó
+		}
+	}
+	// sau khi tìm xong trả về kết quả min nhỏ nhất
+	return min;
+}
+
+// case 7: Tìm giá trị lớn nhất trong mảng
+int TimGiaTriLonNhatTrongMang(Array a, int n) {
+	int max = a[0];
+	for (int i = 0; i < n; i++) {
+		if (a[i] > max) {
+			max = a[i];
+		}
+	}
+	return max;
+}
+
+// case 8: Tìm số lẻ trong mảng
+void TimSoLeTrongMang(Array a, int n) {
+	cout << endl << "\n Xem so le:\t";
+	for (int i = 0; i < n; i++){
+		if (a[i] % 2 != 0) {
+			cout << a[i] << "\t";
+		}
+	}
+}
+
+// case 9: Tìm các số chẵn trong mảng
+void TimSoChanTrongMang(Array a, int n) {
+	cout << endl << "\n Xem so chan:\t";
+	for (int i = 0; i < n; i++) {
+		if (a[i] % 2 == 0) {
+			cout << a[i] << "\t";
+		}
+	}
+}
+
+
+// case 10: Tìm số âm trong mảng
+void TimSoAmTrongMang(Array a, int n) {
+	cout << endl << "\n Xem so am:\t";
+	for (int i = 0; i < n; i++) {
+		if (a[i] < 0) {
+			cout << a[i] << "\t";
+		}
+	}
+}
+
+// case 11: Tìm số dương trong mảng
+void TimSoDuongTrongMang(Array a, int n) {
+	cout << endl << "\n Xem so duong:\t";
+	for (int i = 0; i < n; i++) {
+		if (a[i] > 0) {
+			cout << a[i] << "\t";
+		}
+	}
+}
+
+bool KiemTraSoNguyenTo(int n) {
+	if (n < 2)
+		return false;
+	for (int i = 0; i < sqrt(n); i++)
+		if (n % 2 == 0)
+			return false;
+	return true;
+}
+
+void XuatCacSoNguyenTo(Array a, int n) {
+	cout << endl << "\n Xuat cac so nguyen to:\t";
+	for (int i = 0; i < n; i++)
+	{
+		if (KiemTraSoNguyenTo(a[i])) {
+			cout << a[i] << "\t";
+		}
+	}
+}
+
+bool KiemTraSoChinhPhuong(int n) {
+	int i = sqrt(n);
+	if (i * i == n)
+	{
+		return true;
+	}
+	return false;
+}
+
+void XuatSoChinhPhuong(Array a, int n) {
+	cout << endl << "\n Xuat ra cac so chinh phuong:\t";
+	for (int i = 0; i < n; i++)
+	{
+		if (KiemTraSoChinhPhuong(a[i]) == true)
+		{
+			cout << a[i] << "\t";
+		}
+	}
+}
